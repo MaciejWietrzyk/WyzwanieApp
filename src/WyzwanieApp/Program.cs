696881235a4c27dc09bce590625225a2b675ffd5
MaciejWietrzyk <greenclover@poczta.onet.pl>
2023-02-25 08:55:04 +0100

    pierwszy plik do pushu

---
+++ src/WyzwanieApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WyzwanieApp
+{
+    class Program
+    {
+        public static SavedEmployee save;
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter the employee's name: ");
+            var employee = new Employee(Console.ReadLine());
+            save = new SavedEmployee(employee.Name);
+
+            employee.LowerGrade += OnLowGrade;
+            static void OnLowGrade(object sender, EventArgs args)
+
+            {
+                Console.WriteLine("Oh, no! We should inform boss about this fact");
+            }
+
+            string input = string.Empty;
+            while (true)
+            {
+                Console.WriteLine($"Please eneter grade for {employee.Name}. If you want to quit, press 'q' and ENTER");
+                input = Console.ReadLine();
+
+                if (input == "q")
+                {
+                    break;
+                }
+
+                if (!string.IsNullOrEmpty(input) && input != "q")
+                    save.SaveGrade(input);
+
+                save.GetStatistics();
+                Console.WriteLine("------------");
+            }
+
+        }
+
+    }
+
+}
