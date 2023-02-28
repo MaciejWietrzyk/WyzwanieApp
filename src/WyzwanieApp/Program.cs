b564df9fdd4f1608ec28e9dbc84d46d2b093cd24
MaciejWietrzyk <125646587+MaciejWietrzyk@users.noreply.github.com>
2023-02-28 16:45:10 +0100

    Update Program.cs

--- src/WyzwanieApp/Program.cs
+++ src/WyzwanieApp/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace WyzwanieApp
@@ -8,36 +8,49 @@ namespace WyzwanieApp
 
         static void Main(string[] args)
         {
-
             Console.Write("Enter the employee's name: ");
             var employee = new Employee(Console.ReadLine());
 
             string input = string.Empty;
 
+            bool addGrade = false;
+
             while (true)
             {
-                var color = Console.ForegroundColor;
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($"Please eneter grade (1-6). You can modify grades by \"+\" or \"-\" signs for {employee.Name}.\nYou can not use -1 and +6 grades \nIf you want to quit, press 'q' and ENTER");
-                Console.ForegroundColor = color;
-                input = Console.ReadLine();
-
-                if (input == "q")
+                try
                 {
-                    break;
-                }
+                    var color = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Please eneter grade (1-6). You can modify grades by \"+\" or \"-\" signs for {employee.Name}.\nYou can not use -1 and +6 grades \nIf you want to quit, press 'q' and ENTER");
+                    Console.ForegroundColor = color;
+                    input = Console.ReadLine();
 
-                if (!string.IsNullOrEmpty(input) && input != "q" && CheckFormat(input))
-                {
-                    employee.AddGrade(input);
-                }
-                else
-                {
-                    throw new FormatException("Bad Format");
-                }
+                    if (input == "q")
+                    {
+                        break;
+                    }
 
-                employee.GetStatistics();
-                Console.WriteLine("------------");
+                    if (!string.IsNullOrEmpty(input) && input != "q" && CheckFormat(input))
+                    {
+                        addGrade = employee.AddGrade(input);
+                    }
+                    else
+                    {
+                        addGrade = false;
+                        throw new FormatException("Bad Format");
+                    }
+
+                    if (addGrade)
+                    {
+                        employee.GetStatistics();
+                    }
+                    Console.WriteLine("------------");
+
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
             }
         }
 
@@ -51,10 +64,7 @@ namespace WyzwanieApp
                 if (character > '0' || character < '9' || character == '+' || character == '-')
                     return true;
             }
-
             return false;
         }
-
     }
-
 }
